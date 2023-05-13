219f285d5b7a8552a244ce3eb067ea529a42bb32
ShanifKn <shanifkaraden06@gmail.com>
2023-05-14 00:15:11 +0530

    day one get, delete, update

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/service/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTask().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+
+  toggleRemider(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTask(task).subscribe();
+  }
+}
